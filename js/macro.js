616dbae8b72371a4ee0c361f15146f7671eace31
WesleyFilhoDev <wesleyfilho221204@gmail.com>
2025-08-16 02:27:48 -0300

    calculo da tmb feito

--- js/macro.js
+++ js/macro.js
@@ -13,11 +13,32 @@ function calculateTBM() {
   const age = parseInt(ageValue.value);
   const sexo = sexValue.value;
 
-  if (weight <= 0 || height <= 0 || age <= 0) {
+  if (
+    isNaN(weight) ||
+    weight <= 0 ||
+    isNaN(height) ||
+    height <= 0 ||
+    isNaN(age) ||
+    age <= 0
+  ) {
     resultElement.innerHTML =
       "Por favor, preencha todos os campos corretamente.";
     return;
   }
+
+  let tmb;
+  if (sexo === "masculino") {
+    tmb = 88.362 + 13.397 * weight + 4.799 * height - 5.677 * age;
+  } else if (sexo === "feminino") {
+    tmb = 444.593 + 9.247 * weight + 3.098 * height - 4.33 * age;
+  } else {
+    resultElement.innerHTML = "Selecione o sexo.";
+    return;
+  }
+
+  resultElement.innerHTML = `Sua Taxa Metabólica Basal (TMB) é: <strong>${tmb.toFixed(
+    2
+  )}</strong> calorias/dia.`;
 }
 
 calculateButton.addEventListener("click", calculateTBM);
